cd0284a48de6f3beba968a063289c6e78fe962d9
Aman <amansinghofficial2004@gmail.com>
2026-03-19 13:10:57 +0530

    fetchReq and WebReq

---
+++ src/fetchReq.ts
@@ -0,0 +1,18 @@
+interface Todo {
+    userId: number;
+    id: number;
+    title: string;
+    body: string;
+}
+
+const fetchData = async() => {
+    try{
+        const response = await fetch("https://jsonplaceholder.typicode.com/posts/1");
+        if(!response.ok){
+            throw new Error(`HTTP error ${response.status}`);
+        }
+        const data:Todo = await response.json();
+    } catch(error: any){
+        
+    }
+}
